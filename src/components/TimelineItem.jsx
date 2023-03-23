8e0180ca1f56179bac3515db946f1e28142d2c89
bishwasojha <bishwasojha19@gmail.com>
2023-03-23 16:10:17 +0545

    css refactored

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -3,8 +3,9 @@ import React , { useState } from "react";
 const TimelineItem = ({ data }) =>  {
 
     return (
+        
     <div className="timeline-item">
-        <div className="timeline-item-content">
+        <div className="timeline-item-content" id ={data.id}>
             <h4 className="title" >
                 {data.cardtitle}
             </h4>
